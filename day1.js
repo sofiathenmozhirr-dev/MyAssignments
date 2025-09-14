da4b38e65f2c72d02e493febf811719a4246895b
sofiathenmozhirr-dev <sofiathenmozhirr@gmail.com>
2025-09-14 11:48:27 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,10 @@
+var firstname ="Thenmozhi";
+console.log(firstname +" The Type of  firstname is:  "   +   typeof(firstname));
+var companyname ="Minuscule";
+console.log(companyname  +"  The Type of company name is:  " +  typeof(companyname));
+let mobilenumber ="09090909";
+console.log(mobilenumber +"  The Type of Mobile no is:  " +  typeof(mobilenumber));
+let isautomation = true
+console.log(isautomation +"  The Type of Course  is:  "  +  typeof(isautomation));
+ let hasplaywright 
+ console.log(hasplaywright +"  The Type of Course Name is:  "  +  typeof(hasplaywright));
